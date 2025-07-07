d90a3bde777ca49c2a4ecb7919f672ba17fb808b
Louis <87730062+yacine-louis@users.noreply.github.com>
2025-07-07 11:57:45 +0100

    mux

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000, // (optional) raises the warning threshold
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split large dependencies into separate chunks
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('axios')) {
+              return 'vendor-axios';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
